d42bf95e3e6925d5a22cebfa161601cc63be5067
liuxhg <liuxhg@yonyou.com>
2020-05-28 19:53:42 +0800

    add

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const koa = require('koa');
-const app = new koa();
-
-app.use( async ctx => {
-    ctx.body = "hello koa2"
-})
-
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
